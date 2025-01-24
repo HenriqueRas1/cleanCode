456927118e17eaf804c9132842d6b167a90dc1fd
HenriqueRas1 <henrique.ras@hotmail.com>
2025-01-24 13:54:34 -0300

    feat

--- Nomenclatura de Variaveis.js
+++ Nomenclatura de Variaveis.js
@@ -1,13 +1,16 @@
 // código sujo
-const users = ["Henrique", "Bruno", "Breno"]
+/* const users1 = ["Henrique", "Bruno", "Breno"]
 
 const filtered = users.filter(user => {
     return user.startsWith("B")
 }) 
 
-console.log(filtered)
+console.log(filtered) */
 
 
+
+// código limpo, por conta do nome da variavel, se torna mais simples de ler
+// Evite nomes genericos (ex: data, response, list, args, params)
 const users = ["Henrique", "Bruno", "Breno"]
 
 const usersStartingWithLetterB = users.filter(user => {
